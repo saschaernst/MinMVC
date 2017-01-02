925aa5feba1841de6529472f9fc83d7e081e3ddd
Sascha Ernst <sascha.m.ernst@gmail.com>
2017-01-02 22:51:11 +0100

    added custom signals

--- MinMVC/MinMVC/Mediators/Mediator.cs
+++ MinMVC/MinMVC/Mediators/Mediator.cs
@@ -1,8 +1,12 @@
-﻿namespace MinMVC
+﻿using System.Collections.Generic;
+
+namespace MinMVC
 {
 	public abstract class Mediator<T> : IMediator where T : class, IMediated
 	{
 		protected T mediated;
+
+		readonly IDictionary<ISignal, object> signalMap = new Dictionary<ISignal, object>();
 
 		public void Init (IMediated med)
 		{
@@ -15,6 +19,12 @@
 
 		protected abstract void Register ();
 
+		protected void RegisterSignal (ISignal signal, object listener)
+		{
+			signal.Add(listener);
+			signalMap.AddNewEntry(signal, listener);
+		}
+
 		protected virtual void OnStart ()
 		{
 			
@@ -24,10 +34,24 @@
 		{
 			Unregister();
 
+			ClearSignals();
+
 			mediated.OnRemove -= OnRemove;
 			mediated = null;
 		}
 
-		protected abstract void Unregister ();
+		protected virtual void Unregister ()
+		{
+
+		}
+
+		void ClearSignals ()
+		{
+			foreach (var item in signalMap) {
+				item.Key.Remove(item.Value);
+			}
+
+			signalMap.Clear();
+		}
 	}
 }
---
+++ MinMVC/MinMVC/Signals/Signal.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinMVC
+{
+	public interface ISignal
+	{
+		bool Add (object listener);
+
+		bool Remove (object listener);
+
+		void Reset ();
+	}
+
+	public class Signal : ISignal
+	{
+		readonly HashSet<Action> listeners = new HashSet<Action>();
+
+		public bool Add (object listener)
+		{
+			return Add((Action)listener);
+		}
+
+		public bool Add (Action listener)
+		{
+			return listeners.Add(listener);
+		}
+
+		public bool Remove (object listener)
+		{
+			return Remove((Action)listener);
+		}
+
+		public bool Remove (Action listener)
+		{
+			return listeners.Remove(listener);
+		}
+
+		public void Call ()
+		{
+			foreach (var listener in listeners) {
+				listener();
+			}
+		}
+
+		public void Reset ()
+		{
+			listeners.Clear();
+		}
+	}
+
+	public class Signal<T> : ISignal
+	{
+		readonly HashSet<Action<T>> listeners = new HashSet<Action<T>>();
+
+		public bool Add (object listener)
+		{
+			return Add((Action<T>)listener);
+		}
+
+		public bool Add (Action<T> listener)
+		{
+			return listeners.Add(listener);
+		}
+
+		public bool Remove (object listener)
+		{
+			return Remove((Action<T>)listener);
+		}
+
+		public bool Remove (Action<T> listener)
+		{
+			return listeners.Remove(listener);
+		}
+
+		public void Call (T p1)
+		{
+			foreach (var listener in listeners) {
+				listener(p1);
+			}
+		}
+
+		public void Reset ()
+		{
+			listeners.Clear();
+		}
+	}
+
+	public class Signal<T, U> : ISignal
+	{
+		readonly HashSet<Action<T, U>> listeners = new HashSet<Action<T, U>>();
+
+		public bool Add (object listener)
+		{
+			return Add((Action<T, U>)listener);
+		}
+
+		public bool Add (Action<T, U> listener)
+		{
+			return listeners.Add(listener);
+		}
+
+		public bool Remove (object listener)
+		{
+			return Remove((Action<T, U>)listener);
+		}
+
+		public bool Remove (Action<T, U> listener)
+		{
+			return listeners.Remove(listener);
+		}
+
+		public void Call (T p1, U p2)
+		{
+			foreach (var listener in listeners) {
+				listener(p1, p2);
+			}
+		}
+
+		public void Reset ()
+		{
+			listeners.Clear();
+		}
+	}
+}
